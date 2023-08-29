3eda9a895822983f9ea3d5d3c1dfab497c461368
Player 2 <domain112@yahoo.com>
2023-08-29 16:02:53 -0500

    ingest and chat working for cli and discord

--- pgml-apps/pgml-chat/pgml_chat/halotest.py
+++
@@ -1,6 +0,0 @@
-import time
-from halo import Halo
-
-with Halo(text="Testing Halo for 5 seconds", spinner="dots"):
-    time.sleep(5)
-
--- pgml-apps/pgml-chat/pgml_chat/testbotcodium.py
+++ pgml-apps/pgml-chat/pgml_chat/testbotcodium.py
@@ -1,3 +1,4 @@
+import yaml
 import os
 import asyncio
 import discord
@@ -19,168 +20,173 @@ from halo import Halo
 import ast
 import requests
 
-# Load environment variables
-load_dotenv()
-SYSTEM_PROMPT = os.environ.get("SYSTEM_PROMPT")
-BASE_PROMPT = os.getenv("BASE_PROMPT")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+# Load environment variables from config file
+with open("config.yaml", "r") as f:
+    config = yaml.safe_load(f)
+SYSTEM_PROMPT = config["SYSTEM_PROMPT"]
+BASE_PROMPT = config["BASE_PROMPT"]
+OPENAI_API_KEY = config["OPENAI_API_KEY"]
 
 
 # Add your new code here
-# # logging.basicConfig(level=logging.DEBUG)  # Comment out or remove this line
 class NewlineFilter(logging.Filter):
     def filter(self, record):
         record.msg = record.msg.replace("\\n\\", "\n")
         return True
 
 
-# Create a logger and add the filter
-logger = logging.getLogger()
-logger.addFilter(NewlineFilter())
+def configure_logging():
+    logger = logging.getLogger()
+    logger.addFilter(NewlineFilter())
+    FORMAT = "%(message)s"
+    logging.basicConfig(
+        level=logging.DEBUG,  # Set the logging level to DEBUG
+        format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        handlers=[
+            RichHandler(rich_tracebacks=True)
+        ],  # Use RichHandler for rich, colored output
+    )
 
-# Set up the logging configuration
-FORMAT = "%(message)s"
-logging.basicConfig(
-    level=logging.DEBUG,  # Set the logging level to DEBUG
-    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
-    handlers=[
-        RichHandler(rich_tracebacks=True)
-    ],  # Use RichHandler for rich, colored output
-)
-
-log = logging.getLogger("rich")  # Get the logger named 'rich'
-
-# System prompt placeholder
-system_prompt = "How can I assist you today?"
-
-# Load OpenAI API key from an environment variable or secret management service
-openai_api_key = os.environ.get("OPENAI_API_KEY")
+    log = logging.getLogger("rich")  # Get the logger named 'rich'
 
 
-# Placeholder function for getting the prompt from user input (you'd replace this with your actual function)
-async def get_prompt(user_input):
-    return user_input
+configure_logging()
 
 
-def handler(signum, frame):
-    print("Exiting...")
-    exit(0)
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description="PostgresML Chatbot Builder",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "--collection_name",
+        dest="collection_name",
+        type=str,
+        help="Name of the collection (schema) to store the data in PostgresML database",
+        required=True,
+    )
+    parser.add_argument(
+        "--root_dir",
+        dest="root_dir",
+        type=str,
+        help="Input folder to scan for markdown files. Required for ingest stage. Not required for chat stage",
+    )
+    parser.add_argument(
+        "--stage",
+        dest="stage",
+        choices=["ingest", "chat"],
+        type=str,
+        default="chat",
+        help="Stage to run",
+    )
+    parser.add_argument(
+        "--chat_interface",
+        dest="chat_interface",
+        choices=["cli", "slack", "discord"],
+        type=str,
+        default="cli",
+        help="Chat interface to use",
+    )
+
+    return parser.parse_args()
 
 
-signal.signal(signal.SIGINT, handler)
-
-parser = argparse.ArgumentParser(
-    description="PostgresML Chatbot Builder",
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-)
-parser.add_argument(
-    "--collection_name",
-    dest="collection_name",
-    type=str,
-    help="Name of the collection (schema) to store the data in PostgresML database",
-    required=True,
-)
-parser.add_argument(
-    "--root_dir",
-    dest="root_dir",
-    type=str,
-    help="Input folder to scan for markdown files. Required for ingest stage. Not required for chat stage",
-)
-parser.add_argument(
-    "--stage",
-    dest="stage",
-    choices=["ingest", "chat"],
-    type=str,
-    default="chat",
-    help="Stage to run",
-)
-parser.add_argument(
-    "--chat_interface",
-    dest="chat_interface",
-    choices=["cli", "slack", "discord"],
-    type=str,
-    default="cli",
-    help="Chat interface to use",
-)
-
-args = parser.parse_args()
-
-FORMAT = "%(message)s"
-logging.basicConfig(
-    level=os.environ.get("LOG_LEVEL", "ERROR"),
-    format="%(asctime)s - %(message)s",
-    datefmt="[%X]",
-    handlers=[RichHandler()],
-)
-log = logging.getLogger("rich")
-
-# Load .env file
-load_dotenv(".env")
+args = parse_arguments()
 
 
-# The code is using the `argparse` module to parse command line arguments.
-collection = Collection(args.collection_name)
-stage = args.stage
-chat_interface = args.chat_interface
+async def generate_openai_response(messages):
+    initial_tokens = count_message_tokens(messages)
+    while initial_tokens > INITIAL_TOKEN_LIMIT:
+        most_verbose_message = max(messages, key=lambda x: count_message_tokens([x]))
+        messages.remove(most_verbose_message)
+        initial_tokens = count_message_tokens(messages)
+        print(
+            "[green]Note: Your request is extensive, and some older messages were trimmed to process it. Please provide more specific details or break your request into smaller parts if needed.[/green]"
+        )
+    allowed_completion_tokens = 15000 - initial_tokens
+    completion_tokens = min(MAX_RESPONSE_TOKENS, allowed_completion_tokens)
+    print(f"[green]Available tokens for completion: {completion_tokens}[/green]")
 
-# The above code is retrieving environment variables and assigning their values to various variables.
-database_url = os.environ.get("DATABASE_URL")
-splitter_name = os.environ.get("SPLITTER", "recursive_character")
-splitter_params = os.environ.get(
-    "SPLITTER_PARAMS", {"chunk_size": 1500, "chunk_overlap": 40}
-)
-splitter = Splitter(splitter_name, splitter_params)
-model_name = os.environ.get("MODEL", "intfloat/e5-small")
-model_params = ast.literal_eval(os.environ.get("MODEL_PARAMS", {}))
-model = Model(model_name, "pgml", model_params)
-pipeline = Pipeline(args.collection_name + "_pipeline", model, splitter)
-query_params = ast.literal_eval(os.environ.get("QUERY_PARAMS", {}))
-system_prompt = os.environ.get("SYSTEM_PROMPT")
-base_prompt = os.environ.get("BASE_PROMPT")
-openai_api_key = os.environ.get("OPENAI_API_KEY")
+    openai.api_key = OPENAI_API_KEY
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo-16k-0613",
+            messages=messages,
+            temperature=0.2,
+            max_tokens=completion_tokens,
+            top_p=0.4,
+            frequency_penalty=0,
+            presence_penalty=0,
+        )
 
+    except Exception as e:
+        return ()
+    messages.append(
+        {"role": "ai", "content": response["choices"][0]["message"]["content"]}
+    )
+    total_tokens_after_response = count_message_tokens(messages)
+    print(
+        f"[green]Total tokens after including OpenAI's response: {total_tokens_after_response}[/green]"
+    )
+    if total_tokens_after_response > 16000:
+        print(
+            f"[red]Total tokens {total_tokens_after_response} exceed the model's maximum context length of 16000. Cancelling query.[/red]"
+        )
+        return (
+            "Token limit exceeded after including OpenAI's response. Query cancelled."
+        )
 
-async def upsert_documents(folder: str) -> int:
-    log.info("Scanning " + folder + " for markdown files")
-    md_files = []
-    # root_dir needs a trailing slash (i.e. /root/dir/)
-    for filename in glob.iglob(folder + "/**/*.md", recursive=True):
-        md_files.append(filename)
-
-    log.info("Found " + str(len(md_files)) + " markdown files")
-    documents = []
-    for md_file in track(md_files, description="Extracting text from markdown"):
-        with open(md_file, "r") as f:
-            text = f.read().replace("\x00", "")  # Clean the text data
-            documents.append({"text": text, "id": md_file})
-
-    log.info("Upserting documents into database")
-    await collection.upsert_documents(documents)
-
-    return len(md_files)
-
-
-async def ingest_documents(folder: str):
-    # Add the pipeline to the collection, does nothing if we have already added it
-    await collection.add_pipeline(pipeline)
-    # This will upsert, chunk, and embed the contents in the folder
-    total_docs = await upsert_documents(folder)
-    log.info("Total documents: " + str(total_docs))
+    return response["choices"][0]["message"]["content"]
 
 
-# Initialize the tokenizer and encoder for GPT-4
-enc = encoding_for_model("gpt-4")
+async def chat_cli():
+    print("Welcome to IT Solutions Bot! How can I assist you today?")
+    while True:
+        try:
+            user_input = input("User (Ctrl-C to exit): ")
 
-INITIAL_TOKEN_LIMIT = 14500
-MAX_RESPONSE_TOKENS = 15500
+            # This will fetch relevant data from your database based on user input.
+            context_from_db = await get_prompt(user_input)
+
+            # Start the spinner before making the request.
+            with Halo(text="Processing...", spinner="dots"):
+                time.sleep(5)  # simulate some processing delay
+
+                # Construct the message to be sent to OpenAI.
+                messages = [
+                    {
+                        "role": "system",
+                        "content": "You are consulting the IT Solutions Expert Bot, fine-tuned with a vast knowledge base. This bot is tailored to deliver precise answers for IT-related queries. Accuracy and relevancy are paramount. Ensure you consider the provided context for the best response.",
+                    },
+                    {
+                        "role": "user",
+                        "content": context_from_db + "\n\nQuestion: " + user_input,
+                    },
+                ]
+
+                # Generate response from OpenAI.
+                response = await generate_openai_response(messages)
+
+                print(f"ITS Assistant: {response}")
+
+        except KeyboardInterrupt:
+            print("\nExiting chat...")
+            break
 
 
-def count_message_tokens(messages):
-    """Counts the number of tokens in a list of messages."""
-    token_count = sum([len(enc.encode(message["content"])) for message in messages])
-    print(f"[green]Token count: {token_count}[/green]")
-    return token_count
+def main():
+    if args.stage == "ingest":
+        asyncio.run(run())
+    elif args.stage == "chat":
+        if args.chat_interface == "cli":
+            asyncio.run(chat_cli())
+        elif args.chat_interface == "discord":
+            # Run the bot
+            asyncio.run(run())  # Note: using asyncio.run here
+
+
+main()
 
 
 async def generate_response(messages, openai_api_key, temperature=0.2):
